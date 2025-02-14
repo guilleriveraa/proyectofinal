6ac01ab9f562b6905e9c627fe30a9a2891de843a
guilleriveraa <guilleriveraa12@gmail.com>
2025-02-14 16:52:08 +0100

    Defensa 2

---
+++ Untitled-1.js
@@ -0,0 +1,12 @@
+<html>
+    <head>  
+        <title>ejercio 1</title>
+        <script>
+            console.log(a);
+                        
+        </script>
+    </head>
+    <body>
+        <var></var>
+    </body>
+</html>
